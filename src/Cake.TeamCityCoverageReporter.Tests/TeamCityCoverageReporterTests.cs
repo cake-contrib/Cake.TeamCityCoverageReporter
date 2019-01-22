74ef6e620216cc1141723853903b9120fa87f102
manuc66 <manuc66@gmail.com>
2019-01-22 20:49:45 +0100

    With an empty result file it produce invalid values
    Add failing test

--- src/Cake.TeamCityCoverageReporter.Tests/TeamCityCoverageReporterTests.cs
+++ src/Cake.TeamCityCoverageReporter.Tests/TeamCityCoverageReporterTests.cs
@@ -28,7 +28,33 @@ namespace Cake.TeamCityCoverageReporter.Tests
             var loggedRows = new List<string>();
             mockCakeLog
                 .Setup(x => x.Write(It.IsAny<Verbosity>(), It.IsAny<LogLevel>(), It.IsAny<string>(), It.IsAny<object[]>()))
-                .Callback<Verbosity, LogLevel, string, object[]>((verbosity, logLevel, format, objects) 
+                .Callback<Verbosity, LogLevel, string, object[]>((verbosity, logLevel, format, objects)
+                    => loggedRows.Add(string.Format(format, objects)));
+
+            // When
+            mockCakeContext.Object.TeamCityCoverageReporter(sampleFilePath);
+
+            // Then
+            var result = string.Join("\r\n", loggedRows);
+            var expected = await File.ReadAllTextAsync(sampleExpectedResultsPath).ConfigureAwait(false);
+            result.ShouldBe(expected);
+        }
+        [Fact]
+        public async Task GIVEN_EmptySampleFiles_WHEN_ReporterCalled_THEN_OutputIsAsExpected()
+        {
+            // Given
+            const string sampleFile = @".\Sample2-Empty.xml";
+            const string sampleExpectedResultsPath = @".\Sample2-Empty-Expected.txt";
+            var sampleFilePath = new FilePath(sampleFile);
+            var mockCakeContext = new Mock<ICakeContext>();
+            var mockCakeLog = new Mock<ICakeLog>();
+            mockCakeContext
+                .SetupGet(x => x.Log)
+                .Returns(mockCakeLog.Object);
+            var loggedRows = new List<string>();
+            mockCakeLog
+                .Setup(x => x.Write(It.IsAny<Verbosity>(), It.IsAny<LogLevel>(), It.IsAny<string>(), It.IsAny<object[]>()))
+                .Callback<Verbosity, LogLevel, string, object[]>((verbosity, logLevel, format, objects)
                     => loggedRows.Add(string.Format(format, objects)));
 
             // When
